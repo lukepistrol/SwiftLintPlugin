87f52401cbac791181111cc36d8df895a2b40afe
Simon Jarbrant <simonjarbrant@me.com>
2022-09-01 15:43:43 +0200

    Add support for running as an Xcode build plugin

--- Plugins/SwiftLint/main.swift
+++ Plugins/SwiftLint/main.swift
@@ -30,3 +30,27 @@ struct SwiftLintPlugin: BuildToolPlugin {
         ]
     }
 }
+
+#if canImport(XcodeProjectPlugin)
+import XcodeProjectPlugin
+
+extension SwiftLintPlugin: XcodeBuildToolPlugin {
+    func createBuildCommands(context: XcodePluginContext, target: XcodeTarget) throws -> [Command] {
+        return [
+            .buildCommand(
+                displayName: "Running SwiftLint for \(target.displayName)",
+                executable: try context.tool(named: "swiftlint").path,
+                arguments: [
+                    "lint",
+                    "--in-process-sourcekit",
+                    "--path",
+                    context.xcodeProject.directory.string,
+                    "--config",
+                    "\(context.xcodeProject.directory.string)/.swiftlint.yml"
+                ],
+                environment: [:]
+            )
+        ]
+    }
+}
+#endif
