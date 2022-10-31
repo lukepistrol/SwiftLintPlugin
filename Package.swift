2c46eef281d0ffda4cab1dcbf69e1536fccfbce2
Lukas Pistrol <lukas@pistrol.com>
2022-10-31 12:58:25 +0100

    add command plugin for fixing

--- Package.swift
+++ Package.swift
@@ -13,19 +13,33 @@ let package = Package(
     products: [
         .plugin(
             name: "SwiftLint",
-            targets: ["SwiftLint"]),
-    ],
-    dependencies: [
+            targets: ["SwiftLint"]
+        ),
+        .plugin(
+            name: "SwiftLintFix",
+            targets: ["SwiftLintFix"]
+        ),
     ],
     targets: [
         .binaryTarget(
             name: "SwiftLintBinary",
-            url: "https://github.com/realm/SwiftLint/releases/download/0.47.1/SwiftLintBinary-macos.artifactbundle.zip",
-            checksum: "82ef90b7d76b02e41edd73423687d9cedf0bb9849dcbedad8df3a461e5a7b555"
+            url: "https://github.com/realm/SwiftLint/releases/download/0.49.1/SwiftLintBinary-macos.artifactbundle.zip",
+            checksum: "227258fdb2f920f8ce90d4f08d019e1b0db5a4ad2090afa012fd7c2c91716df3"
         ),
+
         .plugin(
             name: "SwiftLint",
             capability: .buildTool(),
-            dependencies: ["SwiftLintBinary"]),
+            dependencies: ["SwiftLintBinary"]
+        ),
+
+        .plugin(
+            name: "SwiftLintFix",
+            capability: .command(
+                intent: .sourceCodeFormatting(),
+                permissions: [.writeToPackageDirectory(reason: "Fixes fixable lint issues")]
+            ),
+            dependencies: ["SwiftLintBinary"]
+        ),
     ]
 )
---
+++ Plugins/SwiftLintFix/main.swift
@@ -0,0 +1,43 @@
+//
+//  File.swift
+//  
+//
+//  Created by Lukas Pistrol on 31.10.22.
+//
+
+import PackagePlugin
+import Foundation
+
+@main
+struct MyCommandPlugin: CommandPlugin {
+
+    func performCommand(context: PluginContext, arguments: [String]) throws {
+        let tool = try context.tool(named: "swiftlint")
+        let toolUrl = URL(fileURLWithPath: tool.path.string)
+
+        for target in context.package.targets {
+            guard let target = target as? SourceModuleTarget else { continue }
+
+            let process = Process()
+            process.executableURL = toolUrl
+            process.arguments = [
+                "--fix",
+                "--config",
+                "\(context.package.directory.string)/.swiftlint.yml",
+                "\(target.directory)"
+            ]
+
+            print(toolUrl.path, process.arguments!.joined(separator: " "))
+
+            try process.run()
+            process.waitUntilExit()
+
+            if process.terminationReason == .exit && process.terminationStatus == 0 {
+                print("Formatted the source code in \(target.directory).")
+            } else {
+                let problem = "\(process.terminationReason):\(process.terminationStatus)"
+                Diagnostics.error("swift-format invocation failed: \(problem)")
+            }
+        }
+    }
+}
