fc919083ba80d08a2264bd32fd96d6a0362a2464
VishwaiOSDev <vishwa300499@icloud.com>
2022-12-08 20:40:33 +0530

    Updated Version 49.1

--- Package.swift
+++ Package.swift
@@ -23,16 +23,14 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SwiftLintBinary",
-            url: "https://github.com/realm/SwiftLint/releases/download/0.50.1/SwiftLintBinary-macos.artifactbundle.zip",
+            url: "https://github.com/realm/SwiftLint/releases/download/0.49.1/SwiftLintBinary-macos.artifactbundle.zip",
             checksum: "487c57b5a39b80d64a20a2d052312c3f5ff1a4ea28e3cf5556e43c5b9a184c0c"
         ),
-
         .plugin(
             name: "SwiftLint",
             capability: .buildTool(),
             dependencies: ["SwiftLintBinary"]
         ),
-
         .plugin(
             name: "SwiftLintFix",
             capability: .command(
